7043a3cf6f14e0ce9df3a1a1a9db940e3f30e1f7
pe1xoto <157078050+pe1xoto@users.noreply.github.com>
2025-06-18 18:57:41 -0300

    Update app.routes.ts

--- agenda-fullstack/agendaangu2/agenda/src/app/app.routes.ts
+++ agenda-fullstack/agendaangu2/agenda/src/app/app.routes.ts
@@ -4,5 +4,4 @@ import { PessoaComponent } from './pessoa/pessoa.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'pessoas', pathMatch: 'full' },
   { path: 'pessoas', component: PessoaComponent },
-  // Adicione outras rotas conforme necessário
 ];
